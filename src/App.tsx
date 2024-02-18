e71929bafcf1c44bc8755d42d36a9593b8d4ca3c
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-02-18 22:36:30 +0100

    chore: react setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const BetaMap = () => (
+  <svg>
+    <circle cx="150" cy="77" r="40" />
+  </svg>
+);
+
+function App() {
+  return (
+    <>
+      <div>io</div>
+      <BetaMap />
+    </>
+  );
+}
+
+export default App;
